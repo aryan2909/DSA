ef4017d53ccee33698eed7131491923e7d0e4b63
Aryan Sharma <aryansharma2909@gmail.com>
2022-08-29 00:52:57 +0530

    commit5

---
+++ patterns/pattern7.cpp
@@ -0,0 +1,30 @@
+// display:
+// 1
+// 12
+// 123
+// 1234
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int row=1;
+    while(row<=n){
+
+        int col=1;
+        while(col<=row){
+
+            cout<<col;
+            col=col+1;
+        }
+
+        cout<<endl;
+        row=row+1;
+    }
+
+    return 0;
+
+}
